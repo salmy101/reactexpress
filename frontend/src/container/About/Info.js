8b2856e323aa9510f5eafaf72941cd46cd231063
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-10-02 19:32:48 -0600

    working on about  section

---
+++ frontend/src/container/About/Info.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='about__info grid'>
+      <div className='about__box'>
+      <h3 className='about__title'>Eduction</h3>
+      <span className='about__subtitle'>Lighhouse Labs</span>
+      </div>
+
+      <div className='about__box'>
+      <h3 className='about__title'>INFO</h3>
+      <span className='about__subtitle'>INFO</span>
+      </div>
+
+      <div className='about__box'>
+      <h3 className='about__title'>INFO</h3>
+      <span className='about__subtitle'>INFO</span>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Info
